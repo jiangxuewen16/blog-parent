6e98f3d0087a5f4bf81a51670d43ec37cbe3e6ae
蒋雪文 <445251692@qq.com>
2020-10-17 15:12:33 +0800

    Initial commit

---
+++ webapi/src/main/java/com/judream/blog/webapi/WebapiApplication.java
@@ -0,0 +1,13 @@
+package com.judream.blog.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebapiApplication.class, args);
+    }
+
+}

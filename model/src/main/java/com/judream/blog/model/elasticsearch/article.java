b56fa658d3672ca3bc75c5fba9dd13dc68c1469f
蒋雪文 <445251692@qq.com>
2020-10-17 16:27:22 +0800

    添加elasticsearch

---
+++ model/src/main/java/com/judream/blog/model/elasticsearch/article.java
@@ -0,0 +1,94 @@
+package com.judream.blog.model.elasticsearch;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Document(indexName = "article")
+public class article implements Serializable {
+    private static final long serialVersionUID = 6320548148250372657L;
+
+    @Id
+    private String id;
+
+    /**
+     * 类型
+     */
+    private Integer type;
+    /**
+     * 分类ID
+     */
+    @JSONField(name="product_code")
+    private String catId;
+    /**
+     * 标题
+     */
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_max_word")
+    private String title;
+    /**
+     * 简介
+     */
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_max_word")
+    private String description;
+    /**
+     *作者
+     */
+    private String author;
+    /**
+     * 标签
+     */
+    private List<String> labelList;
+    /**
+     * 内容
+     */
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_max_word")
+    private String content;
+    /**
+     * 点赞
+     */
+    private Integer agreeNum;
+    /**
+     * 点踩
+     */
+    private Integer disagreeNum;
+    /**
+     * 排序
+     */
+    private Integer sort;
+    /**
+     * 置顶
+     */
+    private Integer isTop;
+    /**
+     * 背景图
+     */
+    private String bgImg;
+    /**
+     * 评论
+     */
+    private Long commentId;
+
+    /**
+     * 审核状态
+     */
+    private Integer examineStatus;
+
+    private Integer isDeleted;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateAt;
+}
